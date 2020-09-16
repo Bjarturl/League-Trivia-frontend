2f5b6cfbd1e8a9220ba9d79928d319a30248ac2d
Bjarturl <bjarturluk@gmail.com>
2020-09-16 18:40:29 +0000

    api

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const UPDATE_USER = "UPDATE_USER";
-// export const API_URL = "https://league-deildin-api.herokuapp.com/";
-export const API_URL = "http://127.0.0.1:8000/";
+export const API_URL = "https://league-deildin-api.herokuapp.com/";
+// export const API_URL = "http://127.0.0.1:8000/";
